5d426666adae1fb6e5e98fd55363112610f9ae1b
RyokoShiojima <s18C1053UV@s.chibakoudai.jp>
2022-05-09 15:56:32 +0900

    リセットかかって視点が戻る際に撮影されてしまう問題の解決

--- controllers/collect_images/collect_images.py
+++ controllers/collect_images/collect_images.py
@@ -4,26 +4,27 @@ import os
 import sys
 from controller import Supervisor
 import numpy as np
-#import time
+import time
 
 deviceImagePath = os.getcwd()
 supervisor = Supervisor()
 timestep = int(supervisor.getBasicTimeStep())
 camera = supervisor.getDevice('camera_sensor')
 camera.enable(timestep)
+i = 0
 
 try:
     for x in np.arange(-4.3, 4.3, 0.2):
-        supervisor.simulationReset()
         for y in np.arange(-3, 3, 0.2):
             for the in np.arange(-1.57, 1.57, 3.14/10):
                 supervisor.getFromDef('PLAYER').getField('translation').setSFVec3f([x, y, 0.450])
                 supervisor.getFromDef('PLAYER').getField('rotation').setSFRotation([0, 0, 1, the])
-                for i in range(1):
-                    supervisor.step(timestep)
+                supervisor.step(timestep)
                 filename = "x"+format(x,"+.2f")+"_y"+format(y,"+.2f")+"_the"+format(the,"+.3f")+".jpg"
-                camera.saveImage(deviceImagePath + '/images/' + str(filename), 80)
-                #time.sleep(0.5)
+                camera.saveImage(deviceImagePath + '/images/' + str(filename), 80) 
+                i += 1
+        supervisor.simulationReset()
+        supervisor.step(timestep)
 
 except Exception:
     error(f"Unexpected exception in main referee loop: {traceback.format_exc()}", fatal=True)
